6b14afd73e4172f5d0fcb992828325b56ce9785a
Matthieu Jacquot <matthieu.jacquot@talan.com>
2019-05-17 18:59:52 +0200

    improve golang

--- quokkaTemplate/start.sh
+++ quokkaTemplate/start.sh
@@ -3,4 +3,14 @@
 export UID="$(id -u)"
 export GID="$(id -g)"
 
+# get the directory of this script not to be dependant upon where it's invoked from
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [ ! -d "$DIR/home/.emacs.d" ]; then
+    echo "== FIRST RUN =="
+    git clone https://github.com/syl20bnr/spacemacs $DIR/home/.emacs.d
+
+    {{ .runMeFirst_content }}
+fi
+
 docker-compose up --build
